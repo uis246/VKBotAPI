5eabc281477ec0c41e5a377631a0a669bfa09219
uis246 <uis9936@gmail.com>
2018-02-05 23:16:08 +0300

    Just

--- Command.java
+++ Command.java
@@ -7,5 +7,6 @@ import com.vk.api.sdk.objects.messages.Message;
 public interface Command {
 	public void exec(String args) throws Exception;
 	public void exec(String args, int peerId, Message message, VkApiClient vk, UserActor actor) throws Exception;
+	public String description();
 	public byte OPLevel();
 }
